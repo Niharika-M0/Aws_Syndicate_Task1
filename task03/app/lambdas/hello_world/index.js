dae697b92343e95ed34ac3aa2d6dae09f2a2dc1f
Niharika M <niharika_m@epam.com>
2025-03-11 15:20:16 +0530

    update the task

--- task03/app/lambdas/hello_world/index.js
+++ task03/app/lambdas/hello_world/index.js
@@ -1,9 +1,8 @@
 exports.handler = async (event) => {
+    // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify({
-            message: 'Hello from Lambda',
-        }),
+        body: JSON.stringify('Hello from Lambda!'),
     };
     return response;
 };
